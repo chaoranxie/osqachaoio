fc4714279f2d84914cbf68fd8502f2eae350e568
Chaoran Xie <cxie@hubspot.com>
2013-08-30 17:49:22 -0400

    pull in settings_local from https://github.com/joshfinnie/OSQA-Heroku

--- osqa/settings_local.py
+++ osqa/settings_local.py
@@ -1,4 +1,7 @@
 # encoding:utf-8
+import os
+import urlparse
+import sys
 import os.path
 
 SITE_SRC_ROOT = os.path.dirname(__file__)
@@ -13,7 +16,9 @@ logging.basicConfig(
 )
 
 #ADMINS and MANAGERS
-ADMINS = ()
+ADMINS = (
+        #('Your Name', 'your@email'),
+        )
 MANAGERS = ADMINS
 
 DEBUG = False
@@ -23,17 +28,37 @@ DEBUG_TOOLBAR_CONFIG = {
 TEMPLATE_DEBUG = DEBUG
 INTERNAL_IPS = ('127.0.0.1',)
 
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'osqa',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
+try:
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({'NAME': url.path[1:],
+                                     'USER': url.username,
+                                     'PASSWORD': url.password,
+                                     'HOST': url.hostname,
+                                     'PORT': url.port,})
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+
+except Exception:
+        print 'Unexpected error:', sys.exc_info()
+
 
 CACHE_BACKEND = 'file://%s' % os.path.join(os.path.dirname(__file__),'cache').replace('\\','/')
 #CACHE_BACKEND = 'dummy://'
@@ -42,7 +67,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 # This should be equal to your domain name, plus the web application context.
 # This shouldn't be followed by a trailing slash.
 # I.e., http://www.yoursite.com or http://www.hostedsite.com/yourhostapp
-APP_URL = 'http://'
+APP_URL = 'http://YOURURL'
 
 #LOCALIZATIONS
 TIME_ZONE = 'America/New_York'
@@ -55,4 +80,4 @@ LANGUAGE_CODE = 'en'
 DJANGO_VERSION = 1.1
 OSQA_DEFAULT_SKIN = 'default'
 
-DISABLED_MODULES = ['books', 'recaptcha', 'project_badges']
+DISABLED_MODULES = ['books', 'recaptcha', 'project_badges', 'sphinxfulltext', 'pgfulltext']
